08722bf31fe882d987479407bcedad8b7f648906
felipe239989 <felipeanselmodonascimento@gmail.com>
2023-11-17 20:03:51 -0300

    criacao de transacoes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,20 +1,34 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod"
+import crypto from "node:crypto"
 import { knex } from "../database";
 
 export async function transactionsRoutes(app: FastifyInstance) {
 
-    app.get('/hello', async () => {
-        const transaction = await knex('transaction').insert(
-            {
-                id: crypto.randomUUID(),
-                title: 'Transacao de Teste',
-                amount: 1000
-            }
-        ).returning('*')
-    
-        return transaction
+    app.post('/', async (request, reply) => {
+
+        const createTransactionBodySchema = z.object({
+            title: z.string(),
+            amount: z.number(),
+            type: z.enum(['credit', 'debit'])
+        })
+        
+        const { title, amount, type } = createTransactionBodySchema.parse(request.body)
+        // validando os dados do request.body vindo da requisacao pra ver se eles batem com o schema q definimos
+
+        const transaction = await knex('transactions')
+        .insert({
+            id: crypto.randomUUID(),
+            title,
+            amount: type === 'credit' ? amount : amount * -1
+        })
+
+        reply.status(201).send()
+
     })
 
 
 }
+
+//utilizar o zod para validar a requisacao
 
