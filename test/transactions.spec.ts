532e03232dc011a70aa67aaf91b81ba11e989043
felipe239989 <felipeanselmodonascimento@gmail.com>
2023-11-27 15:49:58 -0300

    finalizando teste da aplicacao

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -60,6 +60,77 @@ describe('Transactions routes', () => {
             })
         ])
     })
+
+    test('user should be able to get a specific transaction', async () => {
+
+        //fazer chamada http p/ criar uma nova transacao
+
+        const CreateTransactionResponse = await request(app.server) //servidor do node
+            .post('/transactions')
+            .send({
+                title: 'New Transaction',
+                amount: 5000,
+                type: 'credit'
+            })
+
+        const cookies = CreateTransactionResponse.get('Set-Cookie')
+
+        const listTransactionResponse = await request(app.server)
+            .get('/transactions')
+            .set('Cookie', cookies)
+            .expect(200)
+
+        const transactionId = listTransactionResponse.body.transactions[0].id
+        
+        const getTransactionResponse = await request(app.server)
+            .get(`/transactions/${transactionId}`)
+            .set('Cookie', cookies)
+            .expect(200)
+        
+        console.log(getTransactionResponse.body)
+
+        expect(getTransactionResponse.body.transaction).toEqual(
+            expect.objectContaining({
+                title: 'New Transaction',
+                amount: 5000
+            })
+        )
+    })
+
+    test('user should be able to get the summary', async () => {
+
+        //fazer chamada http p/ criar uma nova transacao
+
+        const CreateTransactionResponse = await request(app.server) //servidor do node
+            .post('/transactions')
+            .send({
+                title: 'Credit Transaction',
+                amount: 5000,
+                type: 'credit'
+            })
+
+        const cookies = CreateTransactionResponse.get('Set-Cookie')
+
+        await request(app.server) //servidor do node
+            .post('/transactions')
+            .set('Cookie', cookies)
+            .send({
+                title: 'Debit Transaction',
+                amount: 2000,
+                type: 'debit'
+            })
+
+        const summaryResponse = await request(app.server)
+            .get('/transactions/summary')
+            .set('Cookie', cookies)
+            .expect(200)
+
+        expect(summaryResponse.body.summary).toEqual(
+            {
+                amount: 3000
+            })
+        
+})
 })
 
 //quando oteste executa tem q ter um ambiente zerado inclusive banco de dados por iso o execSync
